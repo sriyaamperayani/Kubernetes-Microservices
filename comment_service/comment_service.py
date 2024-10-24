b750860b4cb3bc9cf26c3f3e5fe5f0a436585ed7
Sriya Amperayani <54638050+sriyaamperayani@users.noreply.github.com>
2024-10-24 00:18:19 -0700

    Add files via upload

---
+++ comment_service/comment_service.py
@@ -0,0 +1,59 @@
+import sqlite3
+from flask import Flask, render_template, request, url_for, flash, redirect
+from werkzeug.exceptions import abort
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
+
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['title']))
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5003)
+
+'''from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# In-memory database for comments
+comments = []
+
+@app.route('/comments/add', methods=['POST'])
+def add_comment():
+    comment_data = request.get_json()
+    comments.append(comment_data)
+    return jsonify({"message": "Comment added successfully"}), 201
+
+@app.route('/comments', methods=['GET'])
+def get_comments():
+    return jsonify(comments), 200'''
+
+
